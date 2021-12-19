781446965760d4c29099bbfd7ca4373d047f9724
Guilherme Moreira <95253928+GrapUI@users.noreply.github.com>
2021-12-19 08:18:45 -0300

    Sidebar component
    Final component version, remember to run "npm i" and set those node-modules

---
+++ components/NavItem.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {
+    Flex,
+    Text,
+    Icon,
+    Link,
+    Menu,
+    MenuButton
+} from '@chakra-ui/react'
+
+export default function NavItem({ icon, title, active, navSize }) {
+    return (
+        <Flex
+            mt={30}
+            flexDir="column"
+            w="100%"
+            alignItems={navSize == "small" ? "center" : "flex-start"}
+        >
+            <Menu placement="right">
+                <Link
+                    backgroundColor={active && "#cecece"}
+                    p={3}
+                    borderRadius={8}
+                    _hover={{ textDecor: 'none', backgroundColor: "#cecece" }}
+                    w={navSize == "large" && "100%"}
+                >
+                    <MenuButton w="100%">
+                        <Flex>
+                            <Icon as={icon} fontSize="xl" color={active ? "#82AAAD" : "gray.500"} />
+                            <Text ml={5} display={navSize == "small" ? "none" : "flex"}>{title}</Text>
+                        </Flex>
+                    </MenuButton>
+                </Link>
+            </Menu>
+        </Flex>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Sidebar from '../components/Sidebar'
+import { Flex, Text, IconButton } from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <Flex w="100%">
+      <Sidebar />
+    </Flex>
+  )
+}
